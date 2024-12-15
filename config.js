aef45d25d313210643a0bbfb9a68a406ffbd7284
Prasanna Pranith <pranithprasanna7@gmail.com>
2024-12-15 16:37:26 +0530

    Update config.js
    [![Node.js CI](https://github.com/sadiyamin/Lara-MD/actions/workflows/deploy.yml/badge.svg?event=issue_comment)](https://github.com/sadiyamin/Lara-MD/actions/workflows/deploy.yml)

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+uconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+    return text === fault ? true : true
+;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "[![Node.js CI](https://github.com/sadiyamin/Lara-MD/actions/workflows/deploy.yml/badge.svg?event=issue_comment)](https://github.com/sadiyamin/Lara-MD/actions/workflows/deploy.yml)", //
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
